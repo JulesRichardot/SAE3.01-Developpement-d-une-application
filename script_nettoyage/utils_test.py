bee0f4769b504d99ad54d8254dbd8f8958148e42
AhashPARTHIPAN <163518335+AhashPARTHIPAN@users.noreply.github.com>
2024-12-06 17:37:37 +0100

    Update utils_test.py
    utils_test.py : utilise le fichier de test pour justement tester les fonctions de utils.py

--- script_nettoyage/utils_test.py
+++ script_nettoyage/utils_test.py
@@ -5,53 +5,35 @@ Fichier de test des fonctions dans utils.py
 """
 
 # Création d'un DataFrame pour les tests
-data = {
-    'Unnamed: 0': ["3319   ", 3320, ""],
-    'TITRE': [' Game 1', '', ' Game Test 4 '],
-    'REFERENCES (éditeur/distributeur)': ['Distrib ABC', 'Distrib XYZ', 'Distrib jsp     '],
-    'AUTEURS': ['NR', 'Author A', 'Author B'],
-    'DATE DE PARUTION DEBUT': ["ojeo", 1995, 2005],
-    'DATE DE PARUTION FIN': [None, -18, 2025],
-    'INFORMATION DATE': ['boite', 'info', 'info'],
-    'VERSION': ['France', 'USA', 'UK'],
-    'NOMBRE DE JOUEURS': ['1 ou +', '2-4', '1-4'],
-    'AGE INDIQUE (cf colonne B)': ['NR', '10+', '8+'],
-    'MOTS CLES': ['CONNAISSANCES, EDUCATIF', 'ACTION, STRATEGY', 'PUZZLE, LOGIQUE'],
-    'N Boîte': [1, 2, 3],
-    'LOCALISATION_CNJ': ['Connaissance 1', 'Test Location 2', 'Test Location 3'],
-    'MECANISME (cf colonne A)': ['Connaissances', 'Strategie', 'Puzzle'],
-    'Unnamed: 14': ["Puzzle", None, None],
-    'Unnamed: 15': [None, None, "Connaissances"],
-    'Collection d\'origine (cf colonne C)': [None, 'Collection A', 'Collection B'],
-    'Etat': [None, 'Parfait', 'Abime'],
-    'Code barre': [None, '123456789', '987654321']
-}
 
-df = pd.DataFrame(data)
+
+path = "./data/inventaire_perso.xlsx"
+
+data = pd.read_excel(path)
 
 # Test pour excelToId
-print("Testing excelToId:" + "\n" + str(excelToId(df)))
+print("Testing excelToId:" + "\n" + str(excelToId(data)))
 
 # Test pour excelToTitre
-print("\nTesting excelToTitre:" + "\n" + str(excelToTitre(df)))
+print("\nTesting excelToTitre:" + "\n" + str(excelToTitre(data)))
 
 # Test pour excelToRef
-print("\nTesting excelToRef:" + "\n" + str(excelToRef(df)))
+print("\nTesting excelToRef:" + "\n" + str(excelToRef(data)))
 
 # Test pour excelToAuteur
-print("\nTesting excelToAuteur:" + "\n" + str(excelToAuteur(df)))
+print("\nTesting excelToAuteur:" + "\n" + str(excelToAuteur(data)))
 
 # Test pour excelToDateParutionDebut
-print("\nTesting excelToDateParutionDebut:" + "\n" + str(excelToDateParutionDebut(df)))
+print("\nTesting excelToDateParutionDebut:" + "\n" + str(excelToDateParutionDebut(data)))
 
 # Test pour excelToDateParutionFin
-print("\nTesting excelToDateParutionFin:" + "\n" + str(excelToDateParutionFin(df)))
+print("\nTesting excelToDateParutionFin:" + "\n" + str(excelToDateParutionFin(data)))
 
 # Test pour excelToVersion
-print("\nTesting excelToVersion:" + "\n" + str(excelToVersion(df)))
+print("\nTesting excelToVersion:" + "\n" + str(excelToVersion(data)))
 
 # Test pour excelToMotsCles
-print("\nTesting excelToMotsCles:" + "\n" + str(excelToMotsCles(df)))
+print("\nTesting excelToMotsCles:" + "\n" + str(excelToMotsCles(data)))
 
 # Test pour excelToMecanisme
-print("\nTesting excelToMecanisme:" + "\n" + str(excelToMecanisme(df)))
+print("\nTesting excelToMecanisme:" + "\n" + str(excelToMecanisme(data)))
