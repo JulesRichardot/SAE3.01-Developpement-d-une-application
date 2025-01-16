0903b8e4dcc986a087b8dd89f006b78bf83e200d
safiya-ng <161507622+safiya-ng@users.noreply.github.com>
2025-01-16 16:13:07 +0100

    Update view_jeuPresentation.php

--- TEST_SITE/Views/view_jeuPresentation.php
+++ TEST_SITE/Views/view_jeuPresentation.php
@@ -5,43 +5,32 @@
         <!-- Section gauche -->
         <div class="section-gauche">
             <div class="game-image"></div>
-            <p><strong>Nombre d'exemplaires :</strong> <?php echo $nb_boite["nb_boite"]?></p>
+            <p><strong>Nombre d'exemplaires :</strong> <?php echo htmlspecialchars($nb_boite["nb_boite"])?></p>
             <button class="Bouton">Voir les boîtes</button>
         </div>
-
         <!-- Section droite -->
         <div class="info-section">
-            <h1><?php echo $unJeux["titre"]?></h1>
-            <p><span>Éditeur :</span> <?php echo $unJeux["nom_editeur"]?></p>
-            <p><span>Auteur :</span> <?php echo $unJeux["nom_auteur"]?></p>
-            <p><span>Âge : </span><?php echo $unJeux["age_indique"]?> ans</p>
-            <p><span>Nombre de joueurs :</span> <?php echo $unJeux["nombre_de_joueurs"]?> joueurs</p>
-            <p><span>Date de parution :</span> <?php echo $unJeux["date_parution_debut"]?></p>
-            <p><span>Mécanisme :</span><?php echo $unJeux["nom_mecanisme"]?> </p>
-            <p><span>Version :</span> <?php echo $unJeux["version"]?></p>
-            <p><span>Mots-clés :</span> <?php echo $unJeux["mots_cles"]?></p>
+            <h1><?php echo htmlspecialchars($unJeux["titre"])?></h1>
+            <p><span>Éditeur :</span> <?php echo htmlspecialchars($unJeux["nom_editeur"])?></p>
+            <p><span>Auteur :</span> <?php echo htmlspecialchars($unJeux["nom_auteur"])?></p>
+            <p><span>Âge : </span><?php echo htmlspecialchars($unJeux["age_indique"])?> </p>
+            <p><span>Nombre de joueurs :</span> <?php echo htmlspecialchars($unJeux["nombre_de_joueurs"])?></p>
+            <p><span>Date de parution :</span> <?php echo htmlspecialchars($unJeux["date_parution_debut"])?></p>
+            <p><span>Mécanisme :</span><?php echo htmlspecialchars($unJeux["nom_mecanisme"])?> </p>
+            <p><span>Version :</span> <?php echo htmlspecialchars($unJeux["version"])?></p>
+            <p><span>Mots-clés :</span> <?php echo htmlspecialchars($unJeux["mots_cles"])?></p>
         </div>
     </div>
-
     <!-- Section des jeux similaires -->
     <div class="jeux-similaires">
         <h2>Autres jeux similaires :</h2>
         <div class="cards">
+            <?php foreach($jeuSim as $jeu) :?>
             <div class="card">
-                <h3>Titre du jeu</h3>
-                <p>Catégorie du jeu</p>
-                <p>Nombre d'exemplaires disponibles.</p>
+                <h3><a href="?controller=list&action=jeuPresentation&id_jeu=<?= $jeu['id_jeu'] ?>"><?php echo htmlspecialchars($jeu["titre"])?></a></h3>
+                <p>Mecanisme : <?php echo htmlspecialchars($unJeux["nom_mecanisme"])?></p>
             </div>
-            <div class="card">
-                <h3>Titre du jeu</h3>
-                <p>Catégorie du jeu</p>
-                <p>Nombre d'exemplaires disponibles.</p>
-            </div>
-            <div class="card">
-                <h3>Titre du jeu</h3>
-                <p>Catégorie du jeu</p>
-                <p>Nombre d'exemplaires disponibles.</p>
-            </div>
+            <?php endforeach ?>
         </div>
     </div>
 
