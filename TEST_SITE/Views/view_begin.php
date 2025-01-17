3e1eed5c13fcdd8e6139a893d381ff93d8b8d5ee
Lasry <159663031+Lasryy@users.noreply.github.com>
2025-01-17 17:45:52 +0100

    Update view_begin.php

--- TEST_SITE/Views/view_begin.php
+++ TEST_SITE/Views/view_begin.php
@@ -9,17 +9,17 @@
 <body>
     <!-- Tête de page -->
     <header class="nav-header">
-        <a href="?">
+        <a href="index.php">
             <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/e24d432f71ae5fc037e972e78951ca15052e5e8c7824a911375a22f8623cb7b3?placeholderIfAbsent=true&apiKey=d20a853adbac4dde9b424a402120db37" alt="Logo" class="logo">
-        </a>        
+        </a>
         <nav class="nav-links">
-            <a href="?" class="nav-link">Accueil</a>
+            <a href="index.php" class="nav-link">Accueil</a>
             <a href="?controller=list&action=pagination" class="nav-link">Découvrir</a>
-            <a href="monCompte.php" class="nav-link">Mon Compte</a>
             
-            <!-- Gère l'état connecté/déconnecté -->
+            <!-- Vérifie si l'utilisateur est connecté -->
             <?php if (isset($_SESSION['utilisateur'])): ?>
-                <a href="?controller=connexion_inscription&action=deconnexion" class="nav-link">Se déconnecter</a>
+                <a href="?controller=monCompte" class="nav-link">Mon Compte</a>
+                <a href="?controller=connexion_inscription&action=deconnexion" class="nav-link">Se Déconnecter</a>
             <?php else: ?>
                 <a href="?controller=connexion_inscription&action=afficher" class="nav-link">Connexion/Inscription</a>
             <?php endif; ?>
