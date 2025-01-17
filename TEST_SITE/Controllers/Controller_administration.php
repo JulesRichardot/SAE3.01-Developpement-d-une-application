eac5c306783755a3ec62caf65b05dc062541e37f
JulesRichardot <jules.richardot95@gmail.com>
2025-01-17 22:12:47 +0100

    commit controller administration

---
+++ TEST_SITE/Controllers/Controller_administration.php
@@ -0,0 +1,38 @@
+<?php 
+
+class Controller_administration {
+
+    public function action_default()
+    {
+        $this->action_administration();
+    }
+    
+    public function action_administration() {
+        echo "sdnsdv";
+    if (!isset($_SESSION['utilisateur'])) {
+        header('Location: index.php?controller=connexion_inscription&action=afficher&erreur=Veuillez vous connecter.');
+        exit;
+    }
+    $role = $_SESSION['utilisateur']['role'];
+    if ($role !== 'Admin' && $role !== 'Gestionnaire') {
+        header('Location: index.php');
+        exit;
+    }
+
+    $model = Model::getModel();
+    $data = ['role' => $role];
+
+    if ($role === 'Admin') {
+        $data['jeux'] = $model->getJeux();
+        $data['utilisateurs'] = $model->getUtilisateurs();
+        $data['reservations'] = $model->getReservations();
+    } elseif ($role === 'Gestionnaire') {
+        $data['jeux'] = $model->getJeux();
+        $data['reservations'] = $model->getReservations();
+    }
+
+    $this->render("administration", $data);
+}   
+}
+
+?>
