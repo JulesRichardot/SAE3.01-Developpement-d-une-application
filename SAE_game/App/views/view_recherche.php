dc815fb4e2afeed1b2082d65d9de194f6b80c7c9
rania212 <155488513+rania212@users.noreply.github.com>
2025-01-17 18:43:15 +0100

    view_recherche.php

---
+++ SAE_game/App/views/view_recherche.php
@@ -0,0 +1,33 @@
+<?php require_once "view_begin.php" ?>
+
+<div class="container">
+    <h1>Recherche de Jeux</h1>
+    
+    <!-- Formulaire de recherche -->
+    <form class="recherche" action="#" method="GET">
+        <div class="form-group">
+            <label for="mot-cle">Mot-clé</label>
+            <input type="text" id="mot-cle" name="mot-cle" placeholder="Rechercher par titre ou mots-clés">
+        </div>
+        <div class="form-group">
+            <button type="submit" class="Bouton">Rechercher</button>
+        </div>
+    </form>
+
+    <?php if (isset($jeux) && !empty($jeux)): ?>
+        <h2>Résultats de la recherche</h2>
+        <ul>
+            <?php foreach ($jeux as $jeu): ?>
+                <li>
+                    <strong><?php echo htmlspecialchars($jeu['titre']); ?></strong> - <?php echo htmlspecialchars($jeu['mots_cles']); ?>
+                    <br>
+                    <a href="jeu_presentation.php?jeu=<?php echo urlencode($jeu['titre']); ?>">Voir détails</a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    <?php elseif (isset($_GET['mot-cle']) && empty($jeux)): ?>
+        <p>Aucun jeu trouvé pour le mot-clé "<?php echo htmlspecialchars($_GET['mot-cle']); ?>".</p>
+    <?php endif; ?>
+</div>
+
+<?php require_once "view_end.php" ?>
