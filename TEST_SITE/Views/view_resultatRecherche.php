8ffc3d3fed97a5bf43deef0fca2c04fa8f72561b
safiya-ng <161507622+safiya-ng@users.noreply.github.com>
2025-01-18 13:41:53 +0100

    Add files via upload

---
+++ TEST_SITE/Views/view_resultatRecherche.php
@@ -0,0 +1,19 @@
+<?php require_once "view_begin.php" ?>
+<table class="table_rendu">
+        <thead>
+            <tr>
+                <th>Titre</th>
+            </tr>
+        </thead>
+        
+        <tbody>
+            <tr><td><?php if (count($lesTitres) <= 0){echo "aucun titre ne correspond à la recherche";}?></td></tr>
+            <?php foreach ($lesTitres as $jeu): ?>
+                <tr>
+                    <td><a href="?controller=list&action=jeuPresentation&id_jeu=<?= $jeu['id_jeu'] ?>"><?= htmlspecialchars($jeu['titre']) ?></a></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+
+    <?php require_once "view_end.php" ?>
