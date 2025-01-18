d993d2f315cb0ab2d3062bf100d2b531a00fa10c
JulesRichardot <jules.richardot95@gmail.com>
2025-01-18 14:04:16 +0100

    Update Controller_administration.php

--- TEST_SITE/Controllers/Controller_administration.php
+++ TEST_SITE/Controllers/Controller_administration.php
@@ -34,6 +34,55 @@ class Controller_administration extends Controller {
 
         $this->render("administration", $data);
     }   
-}
 
-?>
+    public function action_administrationUtilisateur() {
+        // Vérification du rôle
+        if (!isset($_SESSION['utilisateur'])) {
+            header('Location: index.php?controller=connexion_inscription&action=afficher&erreur=Veuillez vous connecter.');
+            exit;
+        }
+        $role = $_SESSION['utilisateur']['role'];
+        if ($role !== 'Admin' && $role !== 'Gestionnaire') {
+            header('Location: index.php');
+            exit;
+        }
+
+        $model = Model::getModel();
+        // Récupération des données et render
+        if ($role === 'Admin') {
+            $data = [
+                'jeux' => $model->getJeux(),
+                'utilisateurs' => $model->getUtilisateurs(),
+                'reservations' => $model->getReservations(),
+                'role' => $role
+            ];
+        }
+        $this->render("administrationUtilisateur", $data);
+
+    }
+
+    public function action_administrationReservation() {
+        // Vérification du rôle
+        if (!isset($_SESSION['utilisateur'])) {
+            header('Location: index.php?controller=connexion_inscription&action=afficher&erreur=Veuillez vous connecter.');
+            exit;
+        }
+        $role = $_SESSION['utilisateur']['role'];
+        if ($role !== 'Admin' && $role !== 'Gestionnaire') {
+            header('Location: index.php');
+            exit;
+        }
+
+        $model = Model::getModel();
+        // Récupération des données et render
+        if ($role === 'Gestionnaire' || $role === 'Admin' ) {
+            $data = [
+                'jeux' => $model->getJeux(),
+                'reservations' => $model->getReservations(),
+                'role' => $role
+            ];
+        }
+
+        $this->render("administrationReservation", $data);
+    }
+
