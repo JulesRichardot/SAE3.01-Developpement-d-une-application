c6a6a2590cf115c21d5883a3f6c0256f935f0dc7
safiya-ng <161507622+safiya-ng@users.noreply.github.com>
2025-01-18 18:40:32 +0100

    Update Model.php

--- TEST_SITE/Models/Model.php
+++ TEST_SITE/Models/Model.php
@@ -101,8 +101,9 @@ class Model
 
     public function getNbBoite($id_jeu)
     {
-        $query = "SELECT count(*) as nb_boite from boite join jeu On boite.jeu_id = jeu.id_jeu where boite.jeu_id = $id_jeu and jeu.id_jeu = $id_jeu; ";
+        $query = "SELECT count(*) as nb_boite from boite join jeu On boite.jeu_id = jeu.id_jeu where boite.jeu_id = :id_jeu and jeu.id_jeu = :id_jeu; ";
         $req = $this->bd->prepare($query);
+        $req->bindValue(':id_jeu', $id_jeu, PDO::PARAM_INT);
         $req->execute();
         return $req->fetch(PDO::FETCH_ASSOC);
     }
