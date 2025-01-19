0a0822149de1a27aad02b29e5f58e7993261eb3e
AhashPARTHIPAN <163518335+AhashPARTHIPAN@users.noreply.github.com>
2025-01-19 23:09:40 +0100

    Update view_administrationUtilisateur.php

--- app/Views/view_administrationUtilisateur.php
+++ app/Views/view_administrationUtilisateur.php
@@ -24,7 +24,8 @@
                             <td>
                                 <!-- Lien vers l'action form_update_user avec l'ID de l'utilisateur -->
                                 <a href="index.php?controller=set&action=form_update_user&id=<?= $utilisateur['utilisateur_id'] ?>" class="Bouton">Modifier</a>
-                                <a href="index.php?controller=set&action=supprimer&id=<?= $utilisateur['utilisateur_id'] ?>" class="Bouton">Supprimer</a>
+                                <!-- <a href="index.php?controller=set&action=supprimer&id=<?= $utilisateur['utilisateur_id'] ?>" class="Bouton">Supprimer</a> -->
+                                <button class="Bouton Noir" onclick="confirmSuppression(<?= $utilisateur['utilisateur_id'] ?>)">Supprimer</button>
                             </td>
                         </tr>
                     <?php endforeach; ?>
@@ -33,5 +34,14 @@
         </div>
     <?php endif; ?>
 </div>
+
+<script>
+    function confirmSuppression(idUser) {
+        if (confirm("Êtes-vous sûr de vouloir supprimer ce jeu ?")) {
+            // Rediriger vers la suppression si l'utilisateur confirme
+            window.location.href = "?controller=set&action=remove_user&id_user=" + idUser;
+        }
+    }
+</script>
 
 <?php require_once "view_end.php" ?>
